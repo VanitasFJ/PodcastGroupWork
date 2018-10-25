1d724b7d99acd290f973dafff10d4562de5083af
George Chamoon <georgechamoon@gmail.com>
2018-10-25 15:48:55 +0200

    hej

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,32 +29,47 @@
         private void InitializeComponent()
         {
 			this.button1 = new System.Windows.Forms.Button();
+			this.Rubrik = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(109, 385);
+			this.button1.Location = new System.Drawing.Point(22, 208);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
+			// Rubrik
+			// 
+			this.Rubrik.AutoSize = true;
+			this.Rubrik.Font = new System.Drawing.Font("Comic Sans MS", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Rubrik.Location = new System.Drawing.Point(12, 9);
+			this.Rubrik.Name = "Rubrik";
+			this.Rubrik.Size = new System.Drawing.Size(188, 56);
+			this.Rubrik.TabIndex = 1;
+			this.Rubrik.Text = "Podcast ";
+			this.Rubrik.Click += new System.EventHandler(this.label1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Rubrik);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label Rubrik;
 	}
 }
 
